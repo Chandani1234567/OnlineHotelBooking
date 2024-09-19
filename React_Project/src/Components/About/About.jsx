867042d7928b7060d5550750c208c59030bb94a5
aanchal5555 <122430110+aanchal5555@users.noreply.github.com>
2024-09-19 04:07:06 -0700

    deploy

--- React_Project/src/Components/About/About.jsx
+++ React_Project/src/Components/About/About.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 
 const About = () => {
   const navigate = useNavigate();
-
+  event.preventDefault();
   const handleExplore = (event) => {
     
     navigate('/explore');
